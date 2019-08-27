cf466a9c3aa407a45b9dd0f83811c827fb791fa8
johnmadakin <edafeadjeke@gmail.com>
2019-08-27 11:28:15 +0100

    users and admins can view budget and view users

--- server/controllers/BudgetController.js
+++ server/controllers/BudgetController.js
@@ -4,4 +4,18 @@ class BudgetController extends BaseController {
   static createBudget(req, res) {
     return super.successResponse(res, 'Budget Created', 200);
   }
+
+  static viewBudget(req, res) {
+    const budget = {
+      food: 900,
+      transport: 1000,
+    };
+    const user = {
+      name: 'dafe',
+    }
+    return super.successResponse(res, 'Success', 200, {
+      budget,
+      user,
+    });
+  }
 }
